be9c954c5306b05b48780f56bf64c78434eedc16
Cruz Morales <cruz@trespiesdesign.com>
2025-07-31 05:08:23 +0000

    that change worked, however, we're still dealing with a bug where the sc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="kyoto-garden"
-          enableSystem
+          enableSystem={false}
           disableTransitionOnChange
         >
           <AuthProvider>
